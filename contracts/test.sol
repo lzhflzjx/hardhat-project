5befb49c6fd2e2ea52fad4b422dfbc6eaba9d6a2
itw_wangshuo06 <itw_wangshuo06@tkmail.com>
2024-12-06 14:22:47 +0800

    feat:新增基础数据类型

---
+++ contracts/test.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.7;
+
+contract HelloWorld {
+    // 基础数据类型
+    bool boolVal_1 = true;
+    bool boolVal_2 = false;
+
+    uint256 uintVal = 25675665;
+    int256 intVal = -1;
+
+    bytes32 bytesVal = "Hello World";
+    string strVal = "Hellow World";
+
+    address addrVal = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
