2e615f4396430fbe3de1c827ad13e9859feddedf
itw_wangshuo06 <itw_wangshuo06@tkmail.com>
2024-12-12 19:20:28 +0800

    feat:文件位置修改

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition";
 
 const privateKey1 = `0x80940bc4842265d0fcc6a86c449df7e1c4f4ff5f13a7412c3402cd1aaea65bd5`;
