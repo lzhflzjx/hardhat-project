ba5589d76c7151212203fd029bdd3d0dbcd80b39
itw_wangshuo06 <itw_wangshuo06@tkmail.com>
2024-12-13 13:48:15 +0800

    feat:hard配置修正

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,18 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition";
+require("@chainlink/env-enc").config()
 
-const privateKey1 = `0x80940bc4842265d0fcc6a86c449df7e1c4f4ff5f13a7412c3402cd1aaea65bd5`;//钱包测试私钥
+const privateKey1 = process.env.PRIVATE_KEY as string
+const url1 = process.env.SEPOLIA_URL//免费第三方服务商：Alchemy，Infura，QuickNode
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   // defaultNetwork: "rinkeby",
   networks: {
     hardhat: {},
     sepolia: {
-      url: "https://eth-mainnet.alchemyapi.io/v2/0x6d7D8e816d9527d46765Ef3C92722B15b4EdC1b7",//免费第三方服务商：Alchemy，Infura，QuickNode
+      url: url1,
       accounts: [privateKey1],
     },
   },
