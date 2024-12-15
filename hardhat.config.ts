eb964c1ddb75b4b63f532de1aa14fa7f71efb987
lzhflzjx <1601492347@qq.com>
2024-12-15 18:05:20 +0800

    feat:新增代理和配置apiKey

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,13 @@ import "@nomicfoundation/hardhat-ignition";
 require("@chainlink/env-enc").config()
 // require("dotenv").config()
 
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent("http://127.0.0.1:29290");
+setGlobalDispatcher(proxyAgent);
+
 const privateKey1 = process.env.PRIVATE_KEY as string
 const url1 = process.env.SEPOLIA_URL//免费第三方服务商：Alchemy，Infura，QuickNode
+const etherToekn1 = process.env.ETHERSCAN_API_KEY//以太坊浏览器token
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -13,10 +18,13 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     sepolia: {
-      url: url1,
-      accounts: [privateKey1],
+      url: url1,//from-quickNode https://dashboard.quicknode.com/endpoints/411428
+      accounts: [privateKey1],//from 钱包秘钥
     },
   },
+  etherscan:{
+    apiKey:etherToekn1//from-以太坊浏览器 https://etherscan.io/myapikey
+  }
 };
 
 export default config;
