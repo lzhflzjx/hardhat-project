588dd4bd9c35d9d020963a7c53867b6bc319b0eb
lzhflzjx <1601492347@qq.com>
2024-12-15 20:44:07 +0800

    feat:合约部署和打款操作

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,8 @@ const { ProxyAgent, setGlobalDispatcher } = require("undici");
 const proxyAgent = new ProxyAgent("http://127.0.0.1:29290");
 setGlobalDispatcher(proxyAgent);
 
-const privateKey1 = process.env.PRIVATE_KEY as string
+const privateKey1 = process.env.PRIVATE_KEY_1 as string
+const privateKey2 = process.env.PRIVATE_KEY_2 as string
 const url1 = process.env.SEPOLIA_URL//免费第三方服务商：Alchemy，Infura，QuickNode
 const etherToekn1:any = process.env.ETHERSCAN_API_KEY//以太坊浏览器token
 
@@ -19,7 +20,7 @@ const config: HardhatUserConfig = {
     hardhat: {},
     sepolia: {
       url: url1,//from-quickNode https://dashboard.quicknode.com/endpoints/411428
-      accounts: [privateKey1],//from 钱包秘钥
+      accounts: [privateKey1,privateKey2],//from 钱包秘钥
       chainId:11155111 
     },
   },
--- scripts/deployFundMe.js
+++ scripts/deployFundMe.js
@@ -24,6 +24,32 @@ async function main() {
     } else {
         console.log("verification skipped..")
     }
+
+    // init 2 accounts
+    const [firstAccount, secondAccount] = await ethers.getSigners()
+    // fund contract with first account
+    const fundTx = await fundMe.fund({ value: ethers.parseEther("0.1") })//转0.5个ETH
+    await fundTx.wait()
+
+    console.log(`2 accounts address are ${firstAccount.address} and ${secondAccount.address}`)
+
+    // check balance of contract
+    const balanceOfContract = await ethers.provider.getBalance(fundMe.target)//合约的余额
+    console.log(`Balance of the contract is ${balanceOfContract}`)
+
+    // fund contract with second account
+    const fundTxWithSecondAccount = await fundMe.connect(secondAccount).fund({ value: ethers.parseEther("0.2") })    // fundMe.connect(secondAccount)不写的话默认数组第一个
+    await fundTxWithSecondAccount.wait()
+
+    // check balance of contract
+    const balanceOfContractAfterSecondFund = await ethers.provider.getBalance(fundMe.target)
+    console.log(`Balance of the contract is ${balanceOfContractAfterSecondFund}`)
+
+    // check mapping 
+    const firstAccountbalanceInFundMe = await fundMe.funderToAmount(firstAccount.address)
+    const secondAccountbalanceInFundMe = await fundMe.funderToAmount(secondAccount.address)
+    console.log(`Balance of 1 account ${firstAccount.address} amount is ${firstAccountbalanceInFundMe}`)
+    console.log(`Balance of 2 account ${secondAccount.address} amount is ${secondAccountbalanceInFundMe}`)
 }
 
 async function verifyFundMe(fundMeAddr, args) {
