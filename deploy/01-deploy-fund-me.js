6acd9d56dfee57b74a12ead3797296543e5c4440
itw_wangshuo06 <itw_wangshuo06@tkmail.com>
2024-12-17 13:54:42 +0800

    feat:本地部署

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,14 +1,39 @@
-// module.exports = async ({ getNamedAccounts, deployments }) => {
-//     const { firstAccount } = await getNamedAccounts()
-//     const { deploy } = deployments
-//     console.log('this is deploy function',firstAccount)
+const { network } = require("hardhat")
+const {devlopmentChains, networkConfig, LOCK_TIME, CONFIRMATIONS} = require("../helper-hardhat-config")
 
-//     const fundMe = await deploy("FundMe", {
-//         from: firstAccount,
-//         args: [180],
-//         log: true,
-//         // waitConfirmations: confirmations
-//     })
-// }
+module.exports= async({getNamedAccounts, deployments}) => {
+    const {firstAccount} = await getNamedAccounts()
+    const {deploy} = deployments
+    
+    let dataFeedAddr
+    let confirmations
+    if(devlopmentChains.includes(network.name)) {
+        const mockV3Aggregator = await deployments.get("MockV3Aggregator")    
+        dataFeedAddr = mockV3Aggregator.address
+        confirmations = 0
+    } else {
+        dataFeedAddr = networkConfig[network.config.chainId].ethUsdDataFeed
+        confirmations = CONFIRMATIONS
+    }
 
-// module.exports.tags = ["all", "fundme"]
+    const fundMe = await deploy("FundMe", {
+        from: firstAccount,
+        args: [LOCK_TIME, dataFeedAddr],
+        log: true,
+        waitConfirmations: confirmations
+    })
+    // remove deployments directory or add --reset flag if you redeploy contract
+
+    if(hre.network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await hre.run("verify:verify", {
+            address: fundMe.address,
+            constructorArguments: [LOCK_TIME, dataFeedAddr],
+          });        
+    } else {
+        console.log("Network is not sepolia, verification skipped...")
+    }
+
+
+}
+
+module.exports.tags = ["all", "fundme"]
